9358548cec849ae07f921143560f97763911562d
Tim Riscalas <61475187+s454835@users.noreply.github.com>
2021-03-26 20:01:57 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    hungryness = hungryness + 1
+    basic.showNumber(hungryness)
+})
+input.onButtonPressed(Button.B, function () {
+    hungryness = 0
+    basic.showNumber(hungryness)
+})
+let hungryness = 0
+hungryness = 0
